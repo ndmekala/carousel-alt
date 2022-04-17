bc1093d2a5f77b2a5d96246e1eb9cfcbccc801d8
Nirmal Mekala <ndmekala@icloud.com>
2022-04-16 22:41:15 -0500

    Updates

---
+++ precache-manifest.9dce3bbd207c4d1c97985a1e6929e052.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b1bfb72f2306f71cf9c",
+    "url": "/carousel-alt/static/css/main.73400e26.chunk.css"
+  },
+  {
+    "revision": "3b1bfb72f2306f71cf9c",
+    "url": "/carousel-alt/static/js/main.3b1bfb72.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/carousel-alt/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "b36f8949cb984f4794ca",
+    "url": "/carousel-alt/static/js/2.b36f8949.chunk.js"
+  },
+  {
+    "revision": "4de3f95241791021f130",
+    "url": "/carousel-alt/static/js/runtime~main.4de3f952.js"
+  },
+  {
+    "revision": "c5e2a7886711b881ff72ec1400a6e8f2",
+    "url": "/carousel-alt/index.html"
+  }
+];
